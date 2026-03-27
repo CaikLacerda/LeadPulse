ecceec97aaf120ea776a2b30b5c6126c44e8f8f5
CaikLacerda <caiklacerda9@gmail.com>
2026-03-27 09:47:27 -0300

    feat: Criação da tela de login, registro e inicio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   stale_when_importmap_changes
+  layout :layout_by_resource
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -8,5 +9,11 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  private
+
+  def layout_by_resource
+    devise_controller? ? "devise" : "application"
   end
 end
